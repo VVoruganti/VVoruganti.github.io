a6eb7bc8a3688e1e6db679e951df46473afe1293
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-03-25 21:51:23 -0400

    Configuring based on the figma designs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
+import styles from "../styles/index.css"
 
-export default () => <div>Hello world!</div>
+
+
+export default () => (
+<div>
+    <nav>
+        <Link>Blog</Link> 
+        <Link>About Me</Link> 
+        <Link>Projects</Link> 
+    </nav>
+    <div>
+        <h1>Some of My Thoughts</h1> 
+
+    </div>
+</div>
+)
