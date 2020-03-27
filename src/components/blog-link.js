705ef6d42b0c70e4d1bfaeddfa534dd9fbc365e7
Vineeth Voruganti <13438633+VVoruganti@users.noreply.github.com>
2020-03-27 11:59:37 -0400

    Creation of basic css files for component levels and outline of JSX for blog post link

--- src/components/blog-link.js
+++ src/components/blog-link.js
@@ -2,10 +2,19 @@ import React from "react"
 
 class BlogLink extends React.Component() {
     render() {
+        const tags = this.props.tags.map((tag =>
+            <p className="tag">{tag}<p>
+        );
         return (
             <div className="post">
-               <div className="left-post"></div>
+               <div className="left-post">
+                  <p>{this.props.postTitle}</p> 
+               </div>
                <div className="right-post"></div> 
+                  <div className="tags">
+                    {tags}
+                  </div>
+                  <p>{this.props.date}</p>
             </div>
         )
     }
